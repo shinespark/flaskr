094f27c6df9654a5dd6bda44cd721a98adf8094c
shinespark <shinespark64@gmail.com>
2015-11-15 15:41:12 +0900

    finish step3

--- flaskr.py
+++ flaskr.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
 
 
 # configration
@@ -18,6 +19,13 @@ app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
 if __name__ == '__main__':
